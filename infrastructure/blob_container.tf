f4afac4a86dd33d3eb11e5cfaba63106bc1c7ab4
yanislammari <yanis.lammari937@gmail.com>
2026-03-14 02:51:46 +0100

    Add upload blob container

--- infrastructure/blob_container.tf
+++ infrastructure/blob_container.tf
@@ -1,20 +1,5 @@
-resource "azurerm_storage_account" "storage" {
-  name                     = "walletanalyserstorage"
-  resource_group_name      = azurerm_resource_group.walletanalyser_rg.name
-  location                 = azurerm_resource_group.walletanalyser_rg.location
-
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  account_kind             = "StorageV2"
-  min_tls_version          = "TLS1_2"
-
-  blob_properties {
-    versioning_enabled = true
-  }
-
-  tags = {
-    project = "walletanalyser"
-    env     = "prod"
-  }
+resource "azurerm_storage_container" "uploads" {
+  name                  = "uploads"
+  storage_account_id    = azurerm_storage_account.storage.id
+  container_access_type = "blob"
 }
